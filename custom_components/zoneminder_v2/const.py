7a9d7e95a86084095dc71fc5fa4889b7e665cf16
Dan Midthun <dan.midthun@gmail.com>
2022-12-18 14:02:28 -0500

    new file:   custom_components/zoneminder_v2/config_flow.py 	new file:   custom_components/zoneminder_v2/const.py 	new file:   custom_components/zoneminder_v2/manifest.json 	new file:   custom_components/zoneminder_v2/sensor.py 	new file:   hacs.json

---
+++ custom_components/zoneminder_v2/const.py
@@ -0,0 +1,13 @@
+"""Constants for Zoneminder component."""
+
+import sys
+
+DOMAIN = "zoneminder_v2"
+
+DEFAULT_HOST = "localhost"
+
+DEFAULT_PATH = "/zm/"
+DEFAULT_PATH_ZMS = "/zm/cgi-bin/nph-zms"
+DEFAULT_SSL = False
+DEFAULT_TIMEOUT = 10
+DEFAULT_VERIFY_SSL = True
---
+++ custom_components/zoneminder_v2/sensor.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+from dataclasses import dataclass
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorEntity,
+    SensorEntityDescription,
+    SensorStateClass,
+    StateType,
+)
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers import entity_registry
+from homeassistant.helpers.entity import DeviceInfo
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+
+from .const import DOMAIN
