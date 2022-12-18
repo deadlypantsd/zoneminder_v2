f9fcb80c6786064ebf674506ae984b3b3f20fd92
Dan Midthun <dan.midthun@gmail.com>
2022-12-18 14:26:25 -0500

    modified:   custom_components/zoneminder_v2/config_flow.py

--- custom_components/zoneminder_v2/config_flow.py
+++ custom_components/zoneminder_v2/config_flow.py
@@ -31,3 +31,35 @@ DATA_SCHEMA = vol.Schema(
     }
 )
 
+async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> None:
+    """Validate the user input allows us to connect."""
+### TODO
+
+
+class ZoneMinderFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle ZoneMinder config flow."""
+
+    VERSION = 1
+
+    async def async_step_user(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Handle the initial step."""
+        errors = {}
+        if user_input is not None:
+            self._async_abort_entries_match({CONF_HOST: user_input[CONF_HOST]})
+            try:
+                await validate_input(self.hass, user_input)
+                return self.async_create_entry(
+                    title=user_input[CONF_HOST], data=user_input
+                )
+            except CannotConnect:
+                errors["base"] = "cannot_connect"
+
+        return self.async_show_form(
+            step_id="user", data_schema=DATA_SCHEMA, errors=errors
+        )
+
+
+class CannotConnect(exceptions.HomeAssistantError):
+    """Error to indicate we cannot connect."""
